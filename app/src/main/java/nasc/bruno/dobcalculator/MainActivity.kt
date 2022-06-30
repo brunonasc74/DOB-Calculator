8602c1a7f3d2d0db3fe8a216787bd4332c3aa648
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-06-30 05:19:04 -0300

    get age in minutes function created

--- app/src/main/java/nasc/bruno/dobcalculator/MainActivity.kt
+++ app/src/main/java/nasc/bruno/dobcalculator/MainActivity.kt
@@ -1,11 +1,52 @@
 package nasc.bruno.dobcalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var selectedDate: TextView? = null
+    private var ageInMinutes: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.button)
+
+        selectedDate = findViewById(R.id.selectedDate)
+        ageInMinutes = findViewById(R.id.ageInMinutes)
+
+        btn.setOnClickListener { clickDate() }
+    }
+
+    private fun clickDate() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(this, { _, vYear, vMonth, vDay ->
+            val date = "$vDay/${vMonth+1}/$vYear"
+            selectedDate?.text = date
+
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+            val theDate = sdf.parse(date)
+            theDate?.let {
+                val dateInMinutes = theDate.time / 60000
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                currentDate?.let {
+                    val currentDateInMinutes = currentDate.time / 60000
+                    val difference = currentDateInMinutes - dateInMinutes
+                    ageInMinutes?.text = difference.toString()
+                }
+            }
+        }, year, month, day)
+        dpd.datePicker.maxDate = (System.currentTimeMillis() - 86400000); dpd.show()
     }
 }
