8454731ec2af34ca20ba08d41fb0b38e591b599a
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-07-04 02:48:10 -0300

    age in hours function added

--- app/src/main/java/nasc/bruno/dobcalculator/MainActivity.kt
+++ app/src/main/java/nasc/bruno/dobcalculator/MainActivity.kt
@@ -11,6 +11,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private var selectedDate: TextView? = null
     private var ageInMinutes: TextView? = null
+    private var ageInHours: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         val btn: Button = findViewById(R.id.button)
         selectedDate = findViewById(R.id.selectedDate)
         ageInMinutes = findViewById(R.id.ageInMinutes)
+        ageInHours = findViewById(R.id.ageInHours)
 
         btn.setOnClickListener { clickDate() }
     }
@@ -38,6 +40,9 @@ class MainActivity : AppCompatActivity() {
             val currentDateInMinutes =  (date.parse(date.format(System.currentTimeMillis()))?.time ?: 0) / 60000
             val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
             ageInMinutes?.text = differenceInMinutes.toString()
+
+            val differenceInHours = differenceInMinutes / 60
+            ageInHours?.text = differenceInHours.toString()
         }, year, month, day)
         dpd.datePicker.maxDate = (System.currentTimeMillis() - 86400000); dpd.show()
     }
